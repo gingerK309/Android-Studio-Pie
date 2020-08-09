8d78f425f3294380a373ed2a76089d4c2653dfb9
gingerK309 <dlatldydid18@naver.com>
2020-08-09 17:53:21 +0900

    3장

---
+++ Unit3/Automobile.java
@@ -0,0 +1,11 @@
+
+public class Automobile extends Car{
+int seatNum;
+int getSeatNum() {
+	return seatNum;
+}
+void upSpeed(int value) {
+	if(speed+value>=300) speed=300;
+	else speed=speed+value;
+}
+}
---
+++ Unit3/exam10.java
@@ -0,0 +1,11 @@
+
+public class exam10 {
+
+	public static void main(String[] args) {
+		Tiger tiger = new Tiger();
+		Eagle eagle= new Eagle();
+		 tiger.move();
+		 eagle.move();
+	}
+
+}
